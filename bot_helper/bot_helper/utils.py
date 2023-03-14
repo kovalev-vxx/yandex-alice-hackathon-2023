213a6f8588a7b0c226ad7bc730cddc6ad12a377a
Kovalev Valery <kovalev.vxx@gmail.com>
2023-03-14 19:22:22 +0300

    gsheets fix

--- bot_helper/bot_helper/utils.py
+++ bot_helper/bot_helper/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def get_sheet_from_gsheets(sheet_name):
-    link = f"https://tools.aimylogic.com/api/googlesheet2json?sheet={sheet_name}&id={GOOGLE_SHEETS_DB_ID}"
+    # link = f"https://tools.aimylogic.com/api/googlesheet2json?sheet={sheet_name}&id={GOOGLE_SHEETS_DB_ID}"
+    link = f"https://opensheet.elk.sh/{GOOGLE_SHEETS_DB_ID}/{sheet_name}"
     response = requests.get(link).json()
     return response, pd.DataFrame(response)
